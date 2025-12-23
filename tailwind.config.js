62b83dd41517a3590ff5501bd934155c8a21f46c
Juan Gómez <gb.jc@outlook.com>
2025-12-23 16:09:30 +0000

    Set up styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
   theme: {
     extend: {
 			colors: {
-				primary: '#030014'
+				primary: '#030014',
+				secondary: '#151312',
+				light: {
+					100: '#d6c6ff',
+					200: '#a8b5db',
+					300: '#9ca4ab'
+				},
+				dark: {
+					100: '#221f3d',
+					200: '#0f0d23'
+				},
+				accent: '#ab8bff'
 			}
 		},
   },
